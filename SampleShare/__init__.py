b350e3d8306c4ce8531a270987cf7f087a6d9b32
Henna Jethani <hennajethani@gmail.com>
2015-10-10 17:09:59 -0600

    Database updates code

--- SampleShare/__init__.py
+++ SampleShare/__init__.py
@@ -4,3 +4,24 @@ from flask import render_template
 app = Flask(__name__)
 import SampleShare.views
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://hack_cu:hack_cu@localhost/hack_cu_test'
+db = SQLAlchemy(app)
+
+
+class Sample(db.Model):
+    __tablename__ = 'sample'
+    date_time = db.Column(db.DateTime)
+    username = db.Column(db.String(80))
+    id_val = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.VARCHAR(500))
+    uploader = db.Column(db.String(80))
+    file_size = db.Column(db.Float)
+    def __init__(self, username = 'a', date_time = '12/04/2011 12:00:00 AM', id_val = 1, url = 'blah', uploader = 'blahagain', file_size = 30):
+        self.date_time = date_time
+        self.username = username
+        self.id_val = id_val
+        self.url = url
+        self.uploader = uploader
+        self.file_size = file_size
+
+
