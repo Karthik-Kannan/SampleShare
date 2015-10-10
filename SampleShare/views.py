f699f31e5f71d53df959719eef8445e917f508c6
Karthik Kannan <kknicks@gmail.com>
2015-10-10 16:17:46 -0600

    Index Page

--- SampleShare/views.py
+++ SampleShare/views.py
@@ -16,6 +16,11 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+
+@app.route('/')
+def index():
+    return "Hello World"
+
 @app.route('/uploaded_file')
 def uploaded_file():
     return "Hello World"
