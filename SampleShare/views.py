9f374d065285cbd6df5add2dc650573523dff81b
Karthik Kannan <kknicks@gmail.com>
2015-10-10 19:36:02 -0600

    Update functionality

--- SampleShare/views.py
+++ SampleShare/views.py
@@ -1,5 +1,5 @@
 import SampleShare
-from SampleShare import app, Sample
+from SampleShare import app
 from flask import Flask, request, render_template, redirect,url_for
 from werkzeug import secure_filename
 from os import path
